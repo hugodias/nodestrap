b49a3a8aecb9f94756cbe57ce00fff3c3c2f9446
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2013-05-14 12:32:59 -0300

    Added a vendor folder to store generic javascript functions

--- public/javascripts/controllers/users_controller.js
+++ public/javascripts/controllers/users_controller.js
@@ -1,10 +1,3 @@
-function slugify(text) {
-	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
-	text = text.replace(/-/gi, "_");
-	text = text.replace(/\s/gi, "-");
-	return text;
-}
-
 app.controller('UsersCtrl', ['$scope','$location', '$http','parse', '$routeParams',
 	function UsersCtrl($scope, $location, $http, parse, $routeParams) {
 		$scope.user = [];
---
+++ public/javascripts/vendors/utils.js
@@ -0,0 +1,6 @@
+function slugify(text) {
+	text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+	text = text.replace(/-/gi, "_");
+	text = text.replace(/\s/gi, "-");
+	return text;
+}
